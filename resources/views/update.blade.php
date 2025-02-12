f796633d6df9fbfa44dd550bda55031326da480e
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2025-02-12 20:18:06 +0100

    Add cta button to bottom of update page

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,18 +1,21 @@
 <x-frontend-layout>
     <div class="appeal-container pt-4 md:pt-8">
         <a class="text-xl flex gap-2 mb-12" href="/"><x-heroicon-o-arrow-uturn-left class="h-6"/>{{__("pages.update.back")}}</a>
-        {!!
-            \Illuminate\Mail\Markdown::parse(
-                file_get_contents(resource_path('content/update.' . app()->getLocale() . '.md'))
-            )
-        !!}
+        <div class="appeal-update">
+            {!!
+                \Illuminate\Mail\Markdown::parse(
+                    file_get_contents(resource_path('content/update.' . app()->getLocale() . '.md'))
+                )
+            !!}
+        </div>
+        <a href="/#{{__("pages.landing.cta.anchor")}}" class="mt-12 bg-foreground text-background font-inter font-black text-xl md:text-4xl !leading-none text-center uppercase w-full flex justify-center p-4">{{__("pages.landing.cta")}}</a>
         <style>
-            .appeal-container h1 {
+            .appeal-update h1 {
                 font-size: 3.5rem;
                 line-height: 1.1;
                 margin-bottom: 2rem;
             }
-            .appeal-container a {
+            .appeal-update a {
                 text-decoration: underline;
             }
         </style>
