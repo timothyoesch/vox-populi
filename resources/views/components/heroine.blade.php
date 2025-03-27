ce9b6ca9ed895485f86e928c7ded7c1564f9d32a
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-03-27 14:31:08 +0100

    Add Number of supporters

--- resources/views/components/heroine.blade.php
+++ resources/views/components/heroine.blade.php
@@ -2,6 +2,7 @@
     <div class="petition__container petition__container--large py-12 md:py-16">
         <p class="underline mb-2 text-xl md:text-4xl">{{__("petition.heroine.subtitle")}}</p>
         <h1 class="text-4xl md:text-7xl uppercase font-black">{{__("petition.heroine.title")}}</h1>
+        <p class="mt-4 underl">{{__("petition.form.content", ["count" => \App\Models\Supporter::count()])}}</p>
         <a href="#{{__("petition.form.anchor")}}" class="petition__button mt-8 !bg-background !text-foreground text-xl md:text-4xl">
             {{__("petition.heroine.cta.text")}}
             <x-heroicon-o-pencil-square class="w-8 h-8 ml-2"/>
