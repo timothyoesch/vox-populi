4323f5daa7706964d755871d547779d8a9968e48
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-06-07 19:18:57 +0200

    Fix bugs on Safari

--- resources/views/components/arguments/argument.blade.php
+++ resources/views/components/arguments/argument.blade.php
@@ -1,10 +1,10 @@
 <div class="koref-arguments__argument border-t-2 border-b-2 border-accent py-2 md:py-4">
     <p class="koref-arguments__argument-title text-xl md:text-2xl font-bold flex justify-between items-center cursor-pointer">
         <span>{{ $title }}</span>
-        <x-heroicon-o-chevron-down class="h6 md:h-8 mr-2"/>
+        <x-heroicon-o-chevron-down class="h-6 md:h-8 mr-2"/>
     </p>
     <div class="koref-arguments__argument-content--container max-h-0 overflow-hidden">
-        <div class="koref-arguments__argument-content pt-4 opacity-0 translate-y-4">
+        <div class="koref-arguments__argument-content py-4 opacity-0 translate-y-4">
             {!! $slot !!}
         </div>
     </div>
