733c9f46d7d3c1d77d122c178f76ba3897e95bd5
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-06-09 17:59:26 +0200

    Add correct fullscreen setting through JS cause CSS sucks

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,14 @@ import "./elements/menu.js";
 Aos.init({
     once: true,
 });
+
+// Initiatialize Fullscreen class correctly
+window.addEventListener("load", resizeFullscreen);
+window.addEventListener("resize", resizeFullscreen);
+
+const resizeFullscreen = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    const vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty("--vw", `${vw}px`);
+};
