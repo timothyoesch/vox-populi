10a6865eb8f892fc607afb1c5c1b6ab3da0e51be
Timothy Justin Oesch <timothy@kpunkt.ch>
2025-06-09 18:01:24 +0200

    Avoid hoisting

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,12 @@ Aos.init({
 });
 
 // Initiatialize Fullscreen class correctly
-window.addEventListener("load", resizeFullscreen);
-window.addEventListener("resize", resizeFullscreen);
-
 const resizeFullscreen = () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
     const vw = window.innerWidth * 0.01;
     document.documentElement.style.setProperty("--vw", `${vw}px`);
 };
+
+window.addEventListener("load", resizeFullscreen);
+window.addEventListener("resize", resizeFullscreen);
